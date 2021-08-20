56d0639a680340dd987823f6d9eaccc055f8099e
cherro <2221619190@qq.com>
2021-08-20 11:22:00 +0800

    a

--- transmit_sys/AD5543.sv
+++ transmit_sys/AD5543.sv
@@ -5,7 +5,7 @@ module AD5543_tb;
     bit [15:0] data;
     bit clk,sdi,cs_n;
 
-    always #15 aclk=~aclk;  // 33.33MHz
+    always #10 aclk=~aclk;  // 33.33MHz
     initial begin
         areset_n=1'b0;
         repeat(5) @(posedge aclk);
@@ -16,14 +16,21 @@ module AD5543_tb;
         data<=$random();
     end
 
-    AD5543 theAD5543_tb (
+    /*
+    AD5543_32M theAD5543_32M_tb (
+        aclk,areset_n,1'b1,
+        data,
+        clk,sdi,cs_n
+    );
+    */
+    AD5543_48M theAD5543_48M_tb (
         aclk,areset_n,1'b1,
         data,
         clk,sdi,cs_n
     );
 endmodule
 
-module AD5543 #(
+module AD5543_32M #(
     parameter integer DW = 16
 )(
     input wire aclk,areset_n,en,
@@ -66,6 +73,63 @@ module AD5543 #(
             end
             else if(aclk && co15) begin
                 cs_n<=1'b0;
+            end
+        end
+    end
+
+    assign clk=aclk;
+    assign sdi=shift_data[DW-1];
+
+endmodule
+
+module AD5543_48M #(
+    parameter integer DW = 16
+)(
+    input wire aclk,areset_n,en,
+    input wire [DW-1:0] data,
+    output logic clk,sdi,cs_n
+);
+    logic cs;
+    logic co16,co24;
+    logic co16_dly;
+    logic [DW-1:0] shift_data;
+
+    assign cs=~cs_n;
+
+    counter #(16) theCounter16 (aclk,cs,cs,co16);
+    counter #(24) theCounter24 (aclk,areset_n,en,co24);
+
+    always_ff @( posedge aclk ) begin
+        co16_dly<=co16;
+    end
+
+    always_ff @( posedge aclk ) begin
+        if(!areset_n) begin
+            cs_n<=1'b1;
+        end
+        else if(en) begin
+            if(co24) begin
+                cs_n<=1'b0;
+            end
+            else if(co16_dly) begin
+                cs_n<=1'b1;
+            end
+        end
+    end
+
+    always_ff @( posedge aclk ) begin
+        if(!areset_n) begin
+            shift_data<='0;
+        end
+        else if(en) begin
+            if(co24) begin
+                shift_data<=data;
+            end
+            else if(cs) begin
+                shift_data<={shift_data[DW-2:0],1'b0};
+            end
+            else begin
+                shift_data<=shift_data;
             end
         end
     end
