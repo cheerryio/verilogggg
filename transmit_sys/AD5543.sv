f8c0ddc8584b9ce470833bbc02414362ce66a0b5
cherro <2221619190@qq.com>
2021-08-20 14:00:53 +0800

    a

--- transmit_sys/AD5543.sv
+++ transmit_sys/AD5543.sv
@@ -3,9 +3,9 @@
 module AD5543_tb;
     bit aclk,areset_n;
     bit [15:0] data;
-    bit clk,sdi,cs_n;
+    bit sclk,sdi,cs_n;
 
-    always #10 aclk=~aclk;  // 33.33MHz
+    always #5 aclk=~aclk;
     initial begin
         areset_n=1'b0;
         repeat(5) @(posedge aclk);
@@ -23,88 +23,70 @@ module AD5543_tb;
         clk,sdi,cs_n
     );
     */
-    AD5543_48M theAD5543_48M_tb (
+    AD5543_96M theAD5543_96M_tb (
         aclk,areset_n,1'b1,
         data,
-        clk,sdi,cs_n
+        sclk,sdi,cs_n
     );
 endmodule
 
-module AD5543_32M #(
+module AD5543_96M #(
     parameter integer DW = 16
 )(
     input wire aclk,areset_n,en,
     input wire [DW-1:0] data,
-    output logic clk,sdi,cs_n
+    output logic sclk,sdi,cs_n
 );
-    logic aclk_n;
-    logic co15;
-    logic co15_1,co15_2;
-    logic [DW-1:0] shift_data;
-
-    assign aclk_n=~aclk;
-    counter #(DW-1) theCounter15(aclk,areset_n,en,co15);
-    assign co15_1=aclk & co15;
-    assign co15_2=~aclk & co15;
-
-    // drive shift_data
-    always_ff @(posedge aclk or posedge aclk_n or negedge areset_n) begin
-        if(!areset_n) begin
-            shift_data<='0;
-        end
-        else if(en) begin
-            if(aclk_n && co15) begin
-                shift_data<=data;
-            end
-            else if(aclk) begin
-                shift_data<={shift_data[DW-2:0],1'b0};
-            end
-        end
-    end
-
-    // drive cs_n
-    always_ff @( posedge aclk or posedge aclk_n or negedge areset_n) begin
-        if(!areset_n) begin
-            cs_n<=1'b1;
-        end
-        else if(en) begin
-            if(aclk_n && co15) begin
-                cs_n<=1'b1;
-            end
-            else if(aclk && co15) begin
-                cs_n<=1'b0;
-            end
-        end
-    end
-
-    assign clk=aclk;
-    assign sdi=shift_data[DW-1];
-
-endmodule
-
-module AD5543_48M #(
-    parameter integer DW = 16
-)(
-    input wire aclk,areset_n,en,
-    input wire [DW-1:0] data,
-    output logic clk,sdi,cs_n
-);
-    logic cs;
+    logic clk,clk0;
+    logic areset_n_dly,reset_n;
     logic co16,co24;
     logic co16_dly;
     logic [DW-1:0] shift_data;
 
-    assign cs=~cs_n;
-
-    counter #(16) theCounter16 (aclk,cs,cs,co16);
-    counter #(24) theCounter24 (aclk,areset_n,en,co24);
-
     always_ff @( posedge aclk ) begin
-        co16_dly<=co16;
+        if(!areset_n) begin
+            clk<=1'b0;
+        end
+        else if(en) begin
+            clk=~clk;
+        end
     end
 
     always_ff @( posedge aclk ) begin
         if(!areset_n) begin
+            clk0<=1'b0;
+        end
+        else if(en) begin
+            if(co16_dly) begin
+                clk0<=clk0;
+            end
+            else if(cs_n) begin
+                clk0<=1'b0;
+            end
+            else begin
+                clk0<=~clk0;
+            end
+        end
+    end
+
+    always_ff @( posedge aclk ) begin
+        areset_n_dly<=areset_n;
+    end
+
+    always_ff @( posedge aclk ) begin
+        if(!areset_n) begin
+            reset_n<=1'b0;
+        end
+        else if(!areset_n_dly) begin
+            reset_n<=1'b0;
+        end
+        else begin
+            reset_n<=1'b1;
+        end
+    end
+
+    always_ff @( posedge clk ) begin
+        if(!reset_n) begin
             cs_n<=1'b1;
         end
         else if(en) begin
@@ -117,15 +99,22 @@ module AD5543_48M #(
         end
     end
 
-    always_ff @( posedge aclk ) begin
-        if(!areset_n) begin
+    counter #(16) theCounter16 (clk,reset_n&~co24,en,co16);
+    counter #(24) theCounter24 (clk,reset_n,en,co24);
+
+    always_ff @( posedge clk ) begin
+        co16_dly<=co16;
+    end
+
+    always_ff @( posedge clk ) begin
+        if(!reset_n) begin
             shift_data<='0;
         end
         else if(en) begin
             if(co24) begin
                 shift_data<=data;
             end
-            else if(cs) begin
+            else if(!cs_n) begin
                 shift_data<={shift_data[DW-2:0],1'b0};
             end
             else begin
@@ -134,7 +123,7 @@ module AD5543_48M #(
         end
     end
 
-    assign clk=aclk;
+    assign sclk=clk0;
     assign sdi=shift_data[DW-1];
 
 endmodule
